30f3230ca3f033596a59f34ba7d059773a498e10
Damir <kdamir2004@gmail.com>
2024-06-14 09:50:30 +0500

    node_modules забыл убрать, сорри

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 import express from 'express';
-import fetchDNSTransports from './scraper.js';  
+import fetchDNSTransports from './scraper.js';  // Make sure the path to your scraper file is correct
+import cron from 'node-cron';
+
 const app = express();
 const URL = 'https://www.dns-shop.kz/catalog/actual/13963825-4034-4ce1-94d0-41fc78eb6d15/?stock=now-today-tomorrow-later&category=17a892f816404e77';
 
 let parsedTransports = [];
 let fetchTime = new Date().toLocaleString();
 
-app.get('/fetch', (req, res) => {
-    fetchDNSTransports(URL).then(transports => {
+const fetchData = async () => {
+    try {
+        const transports = await fetchDNSTransports(URL);
         fetchTime = new Date().toLocaleString();
         parsedTransports = transports;
-        res.send("Fetching articles completed.");
-    }).catch(error => {
+        console.log("Data fetched at", fetchTime);
+    } catch (error) {
         console.error('Error fetching DNS articles:', error);
-        res.status(500).send('Error fetching articles.');
-    });
+    }
+};
+
+// Schedule the task to run every 2 hours
+cron.schedule('0 */2 * * *', fetchData);
+
+app.get('/fetch', (req, res) => {
+    fetchData()
+        .then(() => res.send("Fetching articles completed."))
+        .catch(error => res.status(500).send('Error fetching articles.'));
 });
 
 app.get('/lastfetch', (req, res) => {
